1bdc0bb524308ef5f46cf852ea87ca149c629d64
Jsinco <jonahmarksinclair@gmail.com>
2024-09-06 18:49:01 -0400

    up

--- src/main/java/dev/jsinco/lumaitems/enums/Tier.java
+++ src/main/java/dev/jsinco/lumaitems/enums/Tier.java
@@ -5,6 +5,7 @@ import net.kyori.adventure.text.Component;
 
 public class Tier {
 
+    public static final Tier DEPRECATED = new Tier("<b><yellow>Deprecated</yellow></b>");
     public static final Tier ASTRAL = new Tier("<b><#AC87FB>Astral</#AC87FB></b>");
     public static final Tier CARNIVAL_2024 = new Tier("<b><#8EC4F7>C<#C7B0E1>a<#FF9CCB>r<#EBC9AC>n<#D7F58D>i<#FFFE8A>v<#FFE978>a<#FFD365>l</b>");
 
